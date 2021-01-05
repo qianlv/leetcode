59702a0410a5b021c9071dd5eee2659c43d05323
qianlv <qianlv@qianlvIMacPro.lan>
2021-01-05 14:25:29 +0800

    Add Rust, and solve permutations 46/47 problem

---
+++ Rust/leetcodes/src/lib.rs
@@ -0,0 +1,10 @@
+pub mod permutations_46;
+pub mod permutations_ii_47;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
---
+++ Rust/leetcodes/src/permutations_46.rs
@@ -0,0 +1,56 @@
+struct Solution;
+use std::collections::HashMap;
+
+impl Solution {
+    fn permute_recursive(
+        nums_count: &mut HashMap<i32, i32>,
+        keys: &Vec<i32>,
+        results: &mut Vec<Vec<i32>>,
+        result: &mut Vec<i32>,
+    ) {
+        let mut zero = true;
+        for key in keys.iter() {
+            // println!("{}", key);
+            if let Some(val) = nums_count.get_mut(key) {
+                if *val > 0 {
+                    *val -= 1;
+                    result.push(*key);
+                    Self::permute_recursive(nums_count, keys, results, result);
+                    result.pop();
+                    // *nums_count.get_mut(key).unwrap() += 1;
+                    zero = false;
+                }
+            }
+        }
+
+        if zero {
+            results.push(result.clone());
+        }
+    }
+
+    pub fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
+        let mut nums_count = HashMap::new();
+        nums.iter().for_each(|x| {
+            let counter = nums_count.entry(*x).or_insert(0);
+            *counter += 1;
+        });
+
+        let mut results: Vec<Vec<i32>> = Vec::new();
+        let mut result = Vec::new();
+        Self::permute_recursive(&mut nums_count, &nums, &mut results, &mut result);
+
+        results
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+    #[test]
+    fn basic() {
+        let results = Solution::permute(vec![1, 1, 2]);
+        for ret in results.iter() {
+            println!("{:?}", ret);
+        }
+    }
+}
