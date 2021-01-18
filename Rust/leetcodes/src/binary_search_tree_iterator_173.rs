932f98f9d26f2c45514337a307cd0dabec2bba1e
qianlv <qianlv7@qq.com>
2021-01-18 19:57:13 +0800

    Binary Search Tree iterator, test

--- Rust/leetcodes/src/binary_search_tree_iterator_173.rs
+++ Rust/leetcodes/src/binary_search_tree_iterator_173.rs
@@ -1,5 +1,5 @@
 // Definition for a binary tree node.
-use std::cell::RefCell;
+use std::cell::{Ref, RefCell};
 use std::rc::Rc;
 
 #[derive(Debug, PartialEq, Eq)]
@@ -21,37 +21,67 @@ impl TreeNode {
 }
 
 struct BSTIterator {
-    stack: Vec<Option<Rc<RefCell<TreeNode>>>>,
+    stack: Vec<Rc<RefCell<TreeNode>>>,
+    // stack: Option<Vec<Rc<RefCell<TreeNode>>>>,
 }
 
 /**
  * `&self` means the method takes an immutable reference.
  * If you need a mutable reference, change it to `&mut self` instead.
  */
+// impl BSTIterator {
+//     fn new(root: Option<Rc<RefCell<TreeNode>>>) -> Self {
+//         let mut iter = BSTIterator { stack: Vec::new() };
+//         if let Some(ref root) = root {
+//             iter.stack.push(Some(root.clone()));
+//             iter.push_left(&root);
+//         }
+//         iter
+//     }
+
+//     fn next(&mut self) -> i32 {
+//         let root = self.stack.pop().expect("BSTIterator non-empty").unwrap();
+//         let ret = root.borrow().val;
+//         if let Some(ref right) = root.borrow().right {
+//             self.stack.push(Some(right.clone()));
+//             self.push_left(&right);
+//         }
+//         ret
+//     }
+
+//     fn push_left(&mut self, root: &Rc<RefCell<TreeNode>>) {
+//         while let Some(ref left) = root.borrow().left {
+//             self.stack.push(Some(left.clone()));
+//             self.push_left(left);
+//         }
+//     }
+
+//     fn has_next(&self) -> bool {
+//         !self.stack.is_empty()
+//     }
+// }
+
 impl BSTIterator {
     fn new(root: Option<Rc<RefCell<TreeNode>>>) -> Self {
         let mut iter = BSTIterator { stack: Vec::new() };
-        if let Some(ref root) = root {
-            iter.stack.push(Some(root.clone()));
-            iter.push_left(&root);
-        }
+        iter.push_left(root);
         iter
     }
 
     fn next(&mut self) -> i32 {
-        let root = self.stack.pop().expect("BSTIterator non-empty").unwrap();
+        let root = self.stack.pop().expect("BSTIterator non-empty");
         let ret = root.borrow().val;
-        if let Some(ref right) = root.borrow().right {
-            self.stack.push(Some(right.clone()));
-            self.push_left(&right);
+        let root = root.borrow();
+        if let Some(right) = root.right.as_ref() {
+            self.push_left(Some(right.clone()));
         }
         ret
     }
 
-    fn push_left(&mut self, root: &Rc<RefCell<TreeNode>>) {
-        while let Some(ref left) = root.borrow().left {
-            self.stack.push(Some(left.clone()));
-            self.push_left(left);
+    fn push_left(&mut self, mut root: Option<Rc<RefCell<TreeNode>>>) {
+        while let Some(left) = root {
+            self.stack.push(left.clone());
+            root = left.borrow().left.as_ref().map(|l| Rc::clone(l));
         }
     }
 
